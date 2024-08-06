8c3246f3498ae2ff79416ec7c81e3aa59a5c1207
Mathias Oterhals Myklebust <mathias@oterbust.no>
2024-08-06 15:58:28 +0200

    feat: landslide risk card

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,7 @@
-import { Badge } from "@/components/ui/badge";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {Card, CardContent, CardTitle} from "@/components/ui/card";
 import Typography from "@/components/ui/typography";
 import { connect, getConnectionInfo } from "@/elastic";
+import { TriangleAlert } from 'lucide-react';
 
 export const revalidate = 20;
 
@@ -65,50 +65,61 @@ export default async function Dashboard() {
 
     // @ts-ignore
     const sensorDangerCount = sensorDangerSearch.aggregations?.["sensors"]["buckets"].length
+    const sensorIsDanger = sensorDangerCount > 0;
 
-    return (
-        <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
-            <Typography variant="h1">Dashboard</Typography>
+    const sensorTotalCount = 20;  // TODO
+    const sensorOkCount = 18;  // TODO
 
-            <div className="grid gap-4 md:gap-8 lg:grid-cols-2">
-                <Card x-chunk="dashboard-01-chunk-5">
-                    <CardHeader>
-                        <CardTitle className="gap-2 flex">
-                            Connection status
-                            {connectionData.connected ? (
-                                <Badge variant="default">Connected</Badge>
-                            ) : (
-                                <Badge variant="destructive">Not connected</Badge>
-                            )}
-                        </CardTitle>
-                    </CardHeader>
-                    <CardContent className="grid gap-8">
-                        {connectionData.connected ? (
-                            <p>
-                                Connected to cluster{" "}
-                                <code className="bg-slate-200 p-1 font-mono rounded-sm">
-                                    {connectionData.clusterName}
-                                </code>
-                                . Counting {numberOfDocuments?.count} documents
-                            </p>
-                        ) : (
-                            <p>Not connected</p>
+    return <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
+        <Typography variant="h1">Dashboard</Typography>
+
+        <div className="grid gap-4 md:gap-8 lg:grid-cols-2">
+            {/*<Card x-chunk="dashboard-01-chunk-5">*/}
+            {/*    <CardHeader>*/}
+            {/*        <CardTitle className="gap-2 flex">*/}
+            {/*            Connection status*/}
+            {/*            {connectionData.connected ? (*/}
+            {/*                <Badge variant="default">Connected</Badge>*/}
+            {/*            ) : (*/}
+            {/*                <Badge variant="destructive">Not connected</Badge>*/}
+            {/*            )}*/}
+            {/*        </CardTitle>*/}
+            {/*    </CardHeader>*/}
+            {/*    <CardContent className="grid gap-8">*/}
+            {/*        {connectionData.connected ? (*/}
+            {/*            <p>*/}
+            {/*                Connected to cluster{" "}*/}
+            {/*                <code className="bg-slate-200 p-1 font-mono rounded-sm">*/}
+            {/*                    {connectionData.clusterName}*/}
+            {/*                </code>*/}
+            {/*                . Counting {numberOfDocuments?.count} documents*/}
+            {/*            </p>*/}
+            {/*        ) : (*/}
+            {/*            <p>Not connected</p>*/}
+            {/*        )}*/}
+            {/*    </CardContent>*/}
+            {/*</Card>*/}
+            <Card className={sensorIsDanger ? "border-red-600 border-4" : ""}>
+                <CardContent>
+                    <div className={"flex flex-row mt-5 gap-5"}>
+                        {sensorIsDanger ? (
+                            <TriangleAlert fill={"#DC2626"} size={50} stroke={"white"} />) : (
+                            <div
+                                className={`me-1 rounded-full h-12 w-12 flex items-center justify-center ${(sensorIsDanger ? 'bg-red-600' : 'bg-emerald-400')}`}>
+                            </div>
                         )}
-                    </CardContent>
-                </Card>
-                <Card>
-                    <CardHeader>
-                        <CardTitle>
-                            <p>{sensorDangerCount > 0 ? "🚨 Landslide risk" : "🟢 No landslide risk"}</p>
-                        </CardTitle>
-                    </CardHeader>
-                    <CardContent>
-                        <p><code className="bg-slate-200 p-1 font-mono rounded-sm">
-                            {sensorDangerCount}
-                        </code> sensor{sensorDangerCount == 1 ? "" : "s"} detecting significant displacement</p>
-                    </CardContent>
-                </Card>
-            </div>
-        </main>
-    );
+                        <div className={"flex flex-col gap-3 mt-1"}>
+                            <CardTitle className={"text-3xl"}>
+                                <p>{sensorIsDanger ? "Rasfare" : "Ingen rasfare"}</p>
+                            </CardTitle>
+                            <p>Dette er basert på data innhentet fra <strong>{sensorOkCount} av {sensorTotalCount}</strong> sensorer.</p>
+                        </div>
+                    </div>
+                    {/*<p><code className="bg-slate-200 p-1 font-mono rounded-sm">*/}
+                    {/*    {sensorDangerCount}*/}
+                    {/*</code> sensor{sensorDangerCount == 1 ? "" : "s"} detecting significant displacement</p>*/}
+                </CardContent>
+            </Card>
+        </div>
+    </main>;
 }
